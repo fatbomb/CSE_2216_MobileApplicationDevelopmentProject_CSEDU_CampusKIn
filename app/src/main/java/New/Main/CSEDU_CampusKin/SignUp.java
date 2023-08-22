565b491ba6aba922d25f5951ef00abf110368a31
fatbomb <joarderaniket@gmail.com>
2023-08-22 06:58:46 +0600

    sign up page design complete may be.

--- app/src/main/java/New/Main/CSEDU_CampusKin/SignUp.java
+++ app/src/main/java/New/Main/CSEDU_CampusKin/SignUp.java
@@ -1,58 +1,27 @@
 package New.Main.CSEDU_CampusKin;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.InputType;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import New.Main.CSEDU_CampusKin.databinding.ActivityMainBinding;
-
-public class MainActivity extends AppCompatActivity {
-
-    ActivityMainBinding binding;
+public class SignUp extends AppCompatActivity {
     boolean isPasswordVisible = false;
+    boolean isPasswordVisible1 = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        //setContentView(R.layout.signup);
-        // Initalize variables
-        final View signup= findViewById(R.id.register_layout);
+        setContentView(R.layout.signup);
+        //getSupportActionBar().hide();
 
-        //initialize animations
-        Animation fade_in= AnimationUtils.loadAnimation(this, R.anim.fade_in);
-        Animation bottom_down=AnimationUtils.loadAnimation(this,R.anim.bottom_down);
-
-        // setting the bottom down animation on top layout
-        binding.topLinearLayout.setAnimation(bottom_down);
-
-        //let's create handler for other layouts
-        Handler handler= new Handler();
-        Runnable runnable= new Runnable() {
-            @Override
-            public void run() {
-                // lets's set fade in animation on other layouts
-                binding.cardView.setAnimation(fade_in);
-                binding.cardView5.setAnimation(fade_in);
-                binding.cardView2.setAnimation(fade_in);
-                binding.cardView3.setAnimation(fade_in);
-                binding.cardView4.setAnimation(fade_in);
-                binding.textView.setAnimation(fade_in);
-                binding.textView2.setAnimation(fade_in);
-                binding.registerLayout.setAnimation(fade_in);
-
-            }
-        };
-
-        handler.postDelayed(runnable,1000);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
         EditText passwordEditText = findViewById(R.id.passwordEditText);
 
 
@@ -82,17 +51,34 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-        signup.setOnClickListener(new View.OnClickListener() {
+        EditText confirmpass = findViewById(R.id.confirmpass);
+
+
+
+        confirmpass.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, SignUp.class));
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_END = 2;
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (passwordEditText.getRight() - passwordEditText.getCompoundDrawables()[DRAWABLE_END].getBounds().width())) {
+                        isPasswordVisible1 = !isPasswordVisible1;
+
+                        if (isPasswordVisible) {
+                            confirmpass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                            //passwordEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.outline_lock_24, 0);
+                            confirmpass.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.outline_lock_24, 0, R.drawable.outline_remove_red_eye_24, 0);
+                        } else {
+                            confirmpass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                            //passwordEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.outline_lock_24, 0);
+                            confirmpass.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.outline_lock_24, 0, R.drawable.hide_pass, 0);
+                        }
+
+                        return true;
+                    }
+                }
+                return false;
             }
         });
-
-
-
-
-
-
     }
 }
