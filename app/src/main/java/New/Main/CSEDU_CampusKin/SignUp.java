9418809a7dd3d18f158396e2630400b300bfe3bf
fatbomb <joarderaniket@gmail.com>
2023-08-22 16:42:24 +0600

    Image can be added now

--- app/src/main/java/New/Main/CSEDU_CampusKin/SignUp.java
+++ app/src/main/java/New/Main/CSEDU_CampusKin/SignUp.java
@@ -1,17 +1,39 @@
 package New.Main.CSEDU_CampusKin;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.InputType;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.widget.ImageView;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class SignUp extends AppCompatActivity {
     boolean isPasswordVisible = false;
     boolean isPasswordVisible1 = false;
+
+    private static final int CAMERA_PERMISSION_REQUEST = 100;
+    private static final int GALLERY_PERMISSION_REQUEST = 101;
+    ImageView profile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +45,8 @@ public class SignUp extends AppCompatActivity {
             actionBar.hide();
         }
         EditText passwordEditText = findViewById(R.id.passwordEditText);
-
+        profile = findViewById(R.id.profile);
+        ImageView addImage = findViewById(R.id.AddImage);
 
 
         passwordEditText.setOnTouchListener(new View.OnTouchListener() {
@@ -54,7 +77,6 @@ public class SignUp extends AppCompatActivity {
         EditText confirmpass = findViewById(R.id.confirmpass);
 
 
-
         confirmpass.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -80,5 +102,99 @@ public class SignUp extends AppCompatActivity {
                 return false;
             }
         });
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showImageOptions(view);
+            }
+        });
+    }
+
+    public void showImageOptions(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Choose an Option")
+                .setItems(new String[]{"Camera", "Gallery"}, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (which == 0) {
+                            requestCameraPermission();
+                        } else if (which == 1) {
+                            requestGalleryPermission();
+                        }
+                    }
+                });
+        builder.show();
+    }
+
+    private void requestCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    CAMERA_PERMISSION_REQUEST);
+        } else {
+            openCamera();
+        }
+    }
+
+    private void requestGalleryPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_MEDIA_IMAGES},
+                    GALLERY_PERMISSION_REQUEST);
+        } else {
+            openGallery();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            } else {
+                // Handle permission denial
+                return;
+            }
+        } else if (requestCode == GALLERY_PERMISSION_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery();
+            } else {
+                // Handle permission denial
+                return;
+            }
+        }
+    }
+
+    private void openCamera() {
+        // Implement camera opening logic here
+        Intent data = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(data, CAMERA_PERMISSION_REQUEST);
+    }
+
+    private void openGallery() {
+        // Implement gallery opening logic here
+        Intent data = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(data, GALLERY_PERMISSION_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_PERMISSION_REQUEST && resultCode == RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            profile.setImageURI(imageUri);
+        } else if (requestCode == CAMERA_PERMISSION_REQUEST && resultCode == RESULT_OK) {
+            Bitmap thumbnail = data.getParcelableExtra("data");
+            profile.setImageBitmap(thumbnail);
+            // Do other work with full size photo saved in locationForPhotos.
+        }
+
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("imageResource", R.id.profile); // Replace with your image resource
     }
 }
+
+
