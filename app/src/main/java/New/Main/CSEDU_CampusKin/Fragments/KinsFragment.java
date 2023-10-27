54325f54eeb7ec7850915bfb0cfeca846ba3fe59
fatbomb <joarderaniket@gmail.com>
2023-10-27 13:19:12 +0600

    Kins page ready..

--- app/src/main/java/New/Main/CSEDU_CampusKin/Fragments/KinsFragment.java
+++ app/src/main/java/New/Main/CSEDU_CampusKin/Fragments/KinsFragment.java
@@ -1,14 +1,40 @@
 package New.Main.CSEDU_CampusKin.Fragments;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import New.Main.CSEDU_CampusKin.Adapters.UserAdapter;
+import New.Main.CSEDU_CampusKin.Model.UserModel;
 import New.Main.CSEDU_CampusKin.R;
 
 public class KinsFragment extends Fragment {
+    private List<String> idList;
+    private RecyclerView recyclerView;
+    private UserAdapter userAdapter;
+    private List<UserModel> mUsers;
 
     public KinsFragment() {
         // Required empty public constructor
@@ -18,6 +44,72 @@ public class KinsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.kins_screen, container, false);
+        View view=inflater.inflate(R.layout.kins_screen, container, false);
+        Toolbar toolbar= view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Followings");
+
+        // Enable the Up button for navigation, if needed
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().onBackPressed();
+            }
+        });
+        getFollowings();
+        recyclerView=view.findViewById(R.id.notificationRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mUsers=new ArrayList<>();
+        userAdapter=new UserAdapter(getContext(), mUsers);
+        idList=new ArrayList<>();
+        recyclerView.setAdapter(userAdapter);
+
+
+        return view;
+    }
+    private void getFollowings() {
+        FirebaseDatabase.getInstance().getReference().child("Follow").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("following").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                idList.clear();
+                for(DataSnapshot snap:snapshot.getChildren()){
+                    idList.add(snap.getKey());
+                }
+                showUsers();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void showUsers() {
+        FirebaseFirestore.getInstance().collection("Users").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                int cnt=0;
+                mUsers.clear();
+                for(QueryDocumentSnapshot dataSnapshot:queryDocumentSnapshots){
+                    UserModel user=dataSnapshot.toObject(UserModel.class);
+                    for(String id:idList){
+                        if(user.getUserID().equals(id)){
+                            mUsers.add(user);
+                            cnt++;
+                        }
+                    }
+
+                }
+                //count.setText(""+cnt);
+                userAdapter.notifyDataSetChanged();
+
+
+
+
+            }
+
+        });
     }
 }
