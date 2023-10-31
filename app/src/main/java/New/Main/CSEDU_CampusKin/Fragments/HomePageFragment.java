256fe1d992a8621c86122b6e575ba35edc29d3fc
fatbomb <joarderaniket@gmail.com>
2023-10-31 12:34:34 +0600

    without following no post is available

--- app/src/main/java/New/Main/CSEDU_CampusKin/Fragments/HomePageFragment.java
+++ app/src/main/java/New/Main/CSEDU_CampusKin/Fragments/HomePageFragment.java
@@ -3,6 +3,7 @@ package New.Main.CSEDU_CampusKin.Fragments;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +13,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -76,14 +82,37 @@ public class HomePageFragment extends Fragment {
         linearLayoutManager.setReverseLayout(true);
         recyclerViewPosts.setLayoutManager(linearLayoutManager);
         postLIst = new ArrayList<>();
-        readposts();
+        followingList=new ArrayList<>();
+        checkFollowingList();
         postAdapter= new PostAdapter(getContext(),postLIst);
         recyclerViewPosts.setAdapter(postAdapter);
 
         //readposts();
 
 
+
         return view;
+    }
+    private void checkFollowingList(){
+        FirebaseDatabase.getInstance().getReference().child("Follow")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child("following").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        followingList.clear();
+                        for(DataSnapshot snap:snapshot.getChildren()){
+                            followingList.add(snap.getKey());
+                        }
+                        readposts();
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
     }
 
     private void readposts() {
@@ -93,7 +122,11 @@ public class HomePageFragment extends Fragment {
                 postLIst.clear();
                 for(QueryDocumentSnapshot snapshot: queryDocumentSnapshots){
                     Post post = snapshot.toObject(Post.class);
-                    postLIst.add(post);
+                    for(String id:followingList){
+                        if(post.getPostedBy().equals(id)||post.getPostedBy().equals(FirebaseAuth.getInstance().getUid())){
+                            postLIst.add(post);
+                        }
+                    }
                 }
                 postAdapter.notifyDataSetChanged();
             }
