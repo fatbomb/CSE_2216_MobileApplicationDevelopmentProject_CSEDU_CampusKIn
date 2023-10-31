549e715a34bae8bcd6b4ba4a97b2aa8ee3257e97
reckless-meherun <meherun2001@gmail.com>
2023-10-31 16:19:24 +0600

    message status (read/unread) and sent time done

--- app/src/main/java/New/Main/CSEDU_CampusKin/Adapters/ChatAdapter.java
+++ app/src/main/java/New/Main/CSEDU_CampusKin/Adapters/ChatAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -14,10 +15,13 @@ import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.common.util.ScopeUtil;
 
+import org.w3c.dom.Text;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import New.Main.CSEDU_CampusKin.Model.ChatMessageModel;
+import New.Main.CSEDU_CampusKin.Model.Post;
 import New.Main.CSEDU_CampusKin.R;
 import New.Main.CSEDU_CampusKin.Utils.FirebaseUtils;
 
@@ -44,15 +48,18 @@ public class ChatAdapter extends FirestoreRecyclerAdapter<ChatMessageModel, Chat
             holder.leftChatLayout.setVisibility(View.GONE);
             holder.rightChatLayout.setVisibility(View.VISIBLE);
             holder.rightChatTextView.setText(model.getMessage());
-            //System.out.println("message unread");
-            if(model.isRead()){
-                System.out.println("Amar meessage seen hoiche");
-                //read icon viesible hobe
 
+            setTime(holder, model);
+
+            if(model.isRead())
+            {
+                holder.sentIcon.setVisibility(View.GONE);
+                holder.readIcon.setVisibility(View.VISIBLE);
             }
-            else{
-                //sent icon visible hobe.
-
+            else
+            {
+                holder.readIcon.setVisibility(View.GONE);
+                holder.sentIcon.setVisibility(View.VISIBLE);
             }
         }
         else
@@ -60,12 +67,7 @@ public class ChatAdapter extends FirestoreRecyclerAdapter<ChatMessageModel, Chat
             holder.rightChatLayout.setVisibility(View.GONE);
             holder.leftChatLayout.setVisibility(View.VISIBLE);
             holder.leftChatTextView.setText(model.getMessage());
-
-            if(model == null)
-                System.out.println("null model");
-
             model.setRead(true);
-            System.out.println("message read");
             Map<String, Object> map = new HashMap<>();
             map.put("read", true);
 
@@ -83,11 +85,45 @@ public class ChatAdapter extends FirestoreRecyclerAdapter<ChatMessageModel, Chat
 
     }
 
+    private void setTime(ChatAdapter.ChatModelViewHolder holder, ChatMessageModel chatMessageModel) {
+        long postTimeMillis = chatMessageModel.getTimestamp().toDate().getTime();
+
+// Get the current time in milliseconds (UTC time)
+        long currentTimeMillis = System.currentTimeMillis();
+
+// Calculate the time difference in seconds
+        long timeDifferenceSeconds = (currentTimeMillis - postTimeMillis) / 1000;
+
+        String timeAgo;
+
+        if (timeDifferenceSeconds < 60) {
+            // Less than a minute ago
+            timeAgo = timeDifferenceSeconds + " sec ago";
+        } else if (timeDifferenceSeconds < 3600) {
+            // Less than an hour ago
+            long minutes = timeDifferenceSeconds / 60;
+            timeAgo = minutes + " min ago";
+        } else if (timeDifferenceSeconds < 86400) {
+            // Less than a day ago
+            long hours = timeDifferenceSeconds / 3600;
+            timeAgo = hours + " hr ago";
+        } else {
+            // Days ago
+            long days = timeDifferenceSeconds / 86400;
+            timeAgo = days + " day ago";
+        }
+
+
+        holder.sentTime.setText(timeAgo);
+    }
+
 
     class ChatModelViewHolder extends RecyclerView.ViewHolder
     {
         LinearLayout leftChatLayout, rightChatLayout;
         TextView leftChatTextView, rightChatTextView;
+        ImageView sentIcon, readIcon;
+        TextView sentTime;
 
         public ChatModelViewHolder(@NonNull View itemView)
         {
@@ -97,6 +133,9 @@ public class ChatAdapter extends FirestoreRecyclerAdapter<ChatMessageModel, Chat
             rightChatLayout = itemView.findViewById(R.id.right_chat_layout);
             leftChatTextView = itemView.findViewById(R.id.left_chat_textview);
             rightChatTextView = itemView.findViewById(R.id.right_chat_textview);
+            sentIcon = itemView.findViewById(R.id.msg_sent_icon);
+            readIcon = itemView.findViewById(R.id.msg_read_icon);
+            sentTime = itemView.findViewById(R.id.sentTime);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
